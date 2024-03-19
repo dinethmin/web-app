67865b19f44aba53e3781e6b5da485e1f9ca0327
Chanuda Dinethmin <chanudadinethmin@gmail.com>
2024-03-19 22:37:23 +0530

    WardenLogin

--- src/WardenLogin.js
+++ src/WardenLogin.js
@@ -7,7 +7,7 @@ const WardenLogin = () => {
             <main className="pa6 black-80 vh-100 w-100">
                 <form className="measure center">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="tc f1 fw6 ph0 mh0">Student Log In</legend>
+                        <legend className="tc f1 fw6 ph0 mh0">Warden Log In</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" for="email-address">Email</label>
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
