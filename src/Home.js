66509b20ffd950e80d308814e9be38e6692261bf
Chanuda Dinethmin <chanudadinethmin@gmail.com>
2024-03-19 23:40:27 +0530

    home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { IoHome } from "react-icons/io5";
 import './Home.css'
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <>
             <header className="bg-dark-gray fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
                 <nav className="fixed f6 fw6 ttu tracked">
+                    <Link className="link dim f3-ns blue dib mr3" to="/Home" title="Home"><IoHome /></Link>
                     <Link className="link dim f4-ns blue dib mr3" to="/Login" title="Login">Login</Link>
                     <Link className="link dim f4-ns blue dib mr3" to="/Signup" title="Signup">Signup</Link>
                     <div className="dropdown link dim f4-ns blue dib mr3">
